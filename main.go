069c2c56067a2bdf1ab0ba75bfc29c74c1a6687d
zhangtbj <zhangtbj@cn.ibm.com>
2019-06-15 16:29:48 +0800

    Add first page

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"html/template"
+	"github.com/labstack/echo"
+)
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e.Renderer = renderer
+
+	e.GET("/hello", Hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "Worldahahahaha")
+}
