4b1e4ef44262064a7fc2afd4e4256e15892b5290
zhangtbj <zhangtbj@cn.ibm.com>
2019-06-16 09:07:13 +0800

    Add list functions

--- go.mod
+++ go.mod
@@ -4,13 +4,12 @@ go 1.12
 
 require (
 	github.com/bluebosh/knap v0.0.0-20190615073538-021f67c20c37
-	github.com/fatih/color v1.7.0
+	github.com/fatih/color v1.7.0 // indirect
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/google/gofuzz v1.0.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.9
-	github.com/spf13/cobra v0.0.5
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	k8s.io/apimachinery v0.0.0-20190221213512-86fb29eff628
 	k8s.io/client-go v2.0.0-alpha.0.0.20181126152608-d082d5923d3c+incompatible
--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/labstack/gommon/color"
-	"io"
-	"net/http"
-	"html/template"
-	"github.com/labstack/echo"
+	knapv1 "github.com/bluebosh/knap/pkg/apis/knap/v1alpha1"
 	knapclientset "github.com/bluebosh/knap/pkg/client/clientset/versioned"
 	"github.com/golang/glog"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/color"
+	"html/template"
+	"io"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // from https://github.com/kubernetes/client-go/issues/345
+	"k8s.io/client-go/tools/clientcmd"
+	"net/http"
+	"strconv"
 )
 
 // var kubeconfig = "/Users/jordan/.bluemix/plugins/container-service/clusters/knative_pipeline/kube-config-dal10-knative_pipeline.yml"
@@ -44,11 +46,12 @@ func main() {
 	e.File("/img/knap.png", "img/knap.png")
 	e.File("/","index.html")
 	e.File("/create","views/create.html")
-	e.GET("/hello", Hello)
+	e.GET("/list", List)
+	e.GET("/createnew", CreateNew)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func Hello(c echo.Context) error {
+func List(c echo.Context) error {
 	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		glog.Fatalf("Error building kubeconfig: %v", err)
@@ -65,5 +68,56 @@ func Hello(c echo.Context) error {
 		app := appLst.Items[i]
 		fmt.Printf("%-30s%-20s%-20s%-20s%-20s\n", app.Name, app.Spec.AppName, app.Status.Ready, fmt.Sprint(app.Status.Instance) + "/" + fmt.Sprint(app.Spec.Size), app.Status.Domain)
 	}
-	return c.Render(http.StatusOK, "hello", appLst.Items[0].Name)
+	return c.Render(http.StatusOK, "list.html", appLst.Items)
+}
+
+func CreateNew(c echo.Context) error {
+	r := c.Request()
+	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %v", err)
+	}
+
+	knapClient, err := knapclientset.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building knap clientset: %v", err)
+	}
+
+
+	size, err:= strconv.ParseInt(r.FormValue("size"),10,32)
+	size32 := int32(size)
+	if err != nil {
+		//glog.Fatalf("Error creating application engine: %s", args[0])
+		fmt.Println("Error parsing size parameter", err)
+	}
+
+	app := &knapv1.Appengine{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      r.FormValue("appName") + "-appengine",
+			Namespace: "default",
+		},
+		Spec:
+		knapv1.AppengineSpec{
+			AppName: r.FormValue("appName"),
+			GitRepo: r.FormValue("gitRepo"),
+			GitRevision: r.FormValue("gitRevision"),
+			// GitWatch: r.FormValue("gitWatch"),
+			Size: size32,
+			PipelineTemplate: r.FormValue("template"),
+		},
+	}
+	_, err = knapClient.KnapV1alpha1().Appengines("default").Create(app)
+
+	if err != nil {
+		//glog.Fatalf("Error creating application engine: %s", args[0])
+		fmt.Println("Error creating application engine", r.FormValue("appName"), err)
+	} else {
+		fmt.Println("Application engine", r.FormValue("appName"), "is created successfully")
+	}
+
+	return c.Render(http.StatusOK, "createDone.html", map[string]interface{}{
+		"name": r.FormValue("appName"),
+	})
+
+
 }
